45dff8b64baa5ccbe1bef6b0409ba438a7591a32
gabriela <gabriela@sertori.com.br>
2022-03-26 00:24:02 -0300

    add: prompt

--- prompt/main.c
+++ prompt/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 22:25:46 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/03/25 22:48:40 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/03/25 23:19:23 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,55 @@
 
 #define clear() printf("\033[H\033[J")
 
+void init()
+{
+    clear();
+    printf("\n\n\n\n******************"
+        "************************");
+    printf("\n\n\n\t****MY SHELL****");
+    printf("\n\n\t-USE AT YOUR OWN RISK-");
+    printf("\n\n\n\n*******************"
+        "***********************");
+    char* username = getenv("USER");
+    printf("\n\n\nUSER is: @%s", username);
+    printf("\n");
+    sleep(1);
+    clear();
+}
+
+int	print_dir(void)
+{
+	char	*cwd = getcwd(cwd, 256);
+
+	printf("MINISHELL:%s$ ", cwd);
+	return (1);
+}
+
+int takeInput(char* str)
+{
+    char* buf;
+
+    buf = readline("");
+    if (strlen(buf) != 0) {
+        strcpy(str, buf);
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
 int	main(void)
 {
-	char	*user = getenv("LOGNAME");
-	char	*machine = getenv("NAME");
-	char	*cwd = getcwd(cwd, 256);
-	
-	clear();
-	while (1)
+	char input[1000];
+	int i = 0;
+	init();
+
+	while (i < 1)
 	{
-		printf("%s@%s:%s", user, machine, cwd);
+		print_dir();
+		if (takeInput(input))
+			continue;
+		i++;
 	}
 	return 0;
 }
