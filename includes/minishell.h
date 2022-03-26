b7b8ea37c2578c30a43a93df7af69e8f620eff76
KrebsCoder <lucasklopess@gmail.com>
2022-03-25 22:19:09 -0300

    add: folders and first file

---
+++ includes/minishell.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/25 22:16:43 by lkrebs-l          #+#    #+#             */
+/*   Updated: 2022/03/25 22:18:35 by lkrebs-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+#endif
