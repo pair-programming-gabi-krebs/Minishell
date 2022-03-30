5aac2d7a1744fc80711880b3fcbb3a4e34198609
KrebsCoder <lucasklopess@gmail.com>
2022-03-29 23:41:16 -0300

    add: struct and funcions prototypes

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,17 +6,29 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 22:16:43 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/03/25 22:44:19 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/03/29 22:46:31 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include "../libft/src/libft.h"
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+
+typedef struct s_init
+{
+	char	**argv;
+	char	**envp;
+	int		argc;
+}	t_init;
+
+int		print_dir(void);
+void	prompt(void);
+void	ft_init(t_init *init, int argc, char *argv[], char *envp[]);
 
 #endif
