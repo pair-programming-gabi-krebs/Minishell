0f120c906c1cc12076f4bb3e552dbbeffac96ed5
gabriela <gabriela@sertori.com.br>
2022-03-30 23:25:53 -0300

    modify: shape as minishell struct

--- prompt/ft_init.c
+++ prompt/ft_init.c
@@ -3,18 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   ft_init.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 22:32:14 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/03/29 22:47:08 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/03/30 21:24:58 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	ft_init(t_init *init, int argc, char *argv[], char *envp[])
+void	ft_init(t_minishell *minishell, int argc, char *argv[], char *envp[])
 {
-	init->argc = argc - 1;
-	init->argv = argv;
-	init->envp = envp;
+	minishell->init.argc = argc - 1;
+	minishell->init.argv = argv;
+	minishell->init.envp = envp;
 }
