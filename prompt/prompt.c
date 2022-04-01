cad3500900d4b5174d0ec4da89eb5596e8cbe798
gabriela <gabriela@sertori.com.br>
2022-03-31 23:28:48 -0300

    modify: change to function previous macro clear()

--- prompt/prompt.c
+++ prompt/prompt.c
@@ -6,13 +6,16 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 22:39:38 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/03/30 21:20:50 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/03/31 21:44:39 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-#define clear() printf("\033[H\033[J")
+void	clear(void)
+{
+	printf("\033[H\033[J");
+}
 
 void	prompt(t_minishell *minishell)
 {
@@ -23,7 +26,11 @@ void	prompt(t_minishell *minishell)
 	{
 		line = readline(print_dir(minishell));
 		if (ft_strlen(line) != 0)
+		{
 			history(line);
+			minishell->lexer.line = ft_strdup(line);
+			lexer(minishell);
+		}
 	}
 	free(minishell->prompt.prompt);
 }
