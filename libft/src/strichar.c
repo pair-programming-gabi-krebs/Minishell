ed58e5b3839f8a994736c92d114e39d13e17e209
gabriela <gabriela@sertori.com.br>
2022-03-31 23:29:19 -0300

    add: strichar inside libft

---
+++ libft/src/strichar.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strichar.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/31 22:04:06 by gcosta-d          #+#    #+#             */
+/*   Updated: 2022/03/31 22:18:09 by gcosta-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	strichar(char *string, int pos, char c)
+{
+	if (!string)
+		return (-1);
+	while (string[pos] != c && string[pos])
+		pos++;
+	if (pos == ft_strlen(string))
+		return (-1);
+	return (pos);
+}
