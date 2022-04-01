9bc778f6674e22382dc67206a2e7df11906cccaa
gabriela <gabriela@sertori.com.br>
2022-03-31 23:28:04 -0300

    remove: define clear() from main.c

--- prompt/main.c
+++ prompt/main.c
@@ -6,13 +6,11 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 22:25:46 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/03/30 21:24:03 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/03/31 21:26:59 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
-
-#define clear() printf("\033[H\033[J")
 
 int	main(int argc, char *argv[], char *envp[])
 {
