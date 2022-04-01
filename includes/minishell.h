35f70c341820f7a7535e3a97335ddcce2c1377dc
gabriela <gabriela@sertori.com.br>
2022-03-31 23:25:49 -0300

    add: lexer functions and struct

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 22:16:43 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/03/30 21:25:20 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/03/31 22:23:24 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,10 +32,19 @@ typedef struct s_prompt
 	char	*prompt;
 }	t_prompt;
 
+typedef struct s_lexer
+{
+	char	*line;
+	char	*infile;
+	char	*outfile;
+}	t_lexer;
+
+
 typedef struct s_minishell
 {
 	t_init		init;
 	t_prompt	prompt;
+	t_lexer		lexer;
 }	t_minishell;
 
 
@@ -44,5 +53,11 @@ char	*print_dir(t_minishell *minishell);
 void	prompt(t_minishell *minishell);
 void	ft_init(t_minishell *minishell, int argc, char *argv[], char *envp[]);
 void	history(char *line);
+
+/* Lexer */
+void	lexer(t_minishell *minishell);
+void	handle_redirect(t_minishell *minishell);
+void	tokenizer(t_minishell *minishell);
+
 
 #endif
