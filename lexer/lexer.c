195d6ac59d27601c8ba66daf11d41426c75558c2
gabriela <gabriela@sertori.com.br>
2022-04-01 23:57:38 -0300

    modify: minishell structure name to ms

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -6,14 +6,14 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 21:26:23 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/03/31 22:58:43 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/04/01 22:56:58 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	lexer(t_minishell *minishell)
+void	lexer(t_ms *ms)
 {
-	tokenizer(minishell);
-	// handle_redirect(minishell);
+	tokenizer(ms);
+	// handle_redirect(ms);
 }
