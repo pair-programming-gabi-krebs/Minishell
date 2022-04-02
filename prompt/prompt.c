e939113f118fb1c1d62d30b142d132d9fe9b9187
gabriela <gabriela@sertori.com.br>
2022-04-02 00:01:21 -0300

    modify: minishell name to ms

--- prompt/prompt.c
+++ prompt/prompt.c
@@ -6,7 +6,7 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 22:39:38 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/03/31 21:44:39 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/04/01 22:55:28 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,20 @@ void	clear(void)
 	printf("\033[H\033[J");
 }
 
-void	prompt(t_minishell *minishell)
+void	prompt(t_ms *ms)
 {
 	char *line;
 
 	clear();
 	while(1)
 	{
-		line = readline(print_dir(minishell));
+		line = readline(print_dir(ms));
 		if (ft_strlen(line) != 0)
 		{
 			history(line);
-			minishell->lexer.line = ft_strdup(line);
-			lexer(minishell);
+			ms->lexer.line = ft_strdup(line);
+			lexer(ms);
 		}
 	}
-	free(minishell->prompt.prompt);
+	free(ms->prompt.prompt);
 }
