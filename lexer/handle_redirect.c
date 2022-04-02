b31cc5cf3c6e3cf1048331f9940e32db5784d2cd
gabriela <gabriela@sertori.com.br>
2022-04-01 23:56:46 -0300

    add: minishell structure name to ms

--- lexer/handle_redirect.c
+++ lexer/handle_redirect.c
@@ -6,7 +6,7 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 21:29:33 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/03/31 22:58:28 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/04/01 22:56:50 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@
  * Verificação se tem outfile
  * Quantidade de outfiles
 */
-void	handle_redirect(t_minishell *minishell)
+void	handle_redirect(t_ms *ms)
 {
 	int	infile_redirect;
 
-	infile_redirect = strichar(minishell->lexer.line, 0, '<');
+	infile_redirect = strichar(ms->lexer.line, 0, '<');
 	if (infile_redirect != -1)
 	{
-		while (minishell->lexer.line[infile_redirect + 1] == ' ')
+		while (ms->lexer.line[infile_redirect + 1] == ' ')
 			infile_redirect++;
 
 	}
