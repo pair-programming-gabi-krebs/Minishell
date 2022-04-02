d46cfdf06c8cb6c9d90bdc901a3534e1a916e894
gabriela <gabriela@sertori.com.br>
2022-04-02 00:00:21 -0300

    modify: minishell name to ms

--- prompt/ft_init.c
+++ prompt/ft_init.c
@@ -6,15 +6,15 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 22:32:14 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/03/30 21:24:58 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/04/01 22:55:11 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	ft_init(t_minishell *minishell, int argc, char *argv[], char *envp[])
+void	ft_init(t_ms *ms, int argc, char *argv[], char *envp[])
 {
-	minishell->init.argc = argc - 1;
-	minishell->init.argv = argv;
-	minishell->init.envp = envp;
+	ms->init.argc = argc - 1;
+	ms->init.argv = argv;
+	ms->init.envp = envp;
 }
