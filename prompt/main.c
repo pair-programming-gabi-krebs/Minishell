640e2c711a8c83cbfef4dde34a710242e162523c
gabriela <gabriela@sertori.com.br>
2022-04-02 00:00:39 -0300

    modify: minishell name to ms

--- prompt/main.c
+++ prompt/main.c
@@ -6,7 +6,7 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 22:25:46 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/03/31 21:26:59 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/04/01 22:54:55 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int	main(int argc, char *argv[], char *envp[])
 {
-	t_minishell	minishell;
+	t_ms	ms;
 
-	ft_init(&minishell, argc, argv, envp);
-	prompt(&minishell);
+	ft_init(&ms, argc, argv, envp);
+	prompt(&ms);
 	return (0);
 }
