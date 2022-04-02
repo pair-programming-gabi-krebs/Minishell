1c949163d6e7fcdd731a3c10ed7bf4020047d524
gabriela <gabriela@sertori.com.br>
2022-04-02 00:00:59 -0300

    modify: minishell name to ms

--- prompt/print_dir.c
+++ prompt/print_dir.c
@@ -6,13 +6,13 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 22:45:16 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/03/30 21:21:59 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/04/01 22:56:11 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-char	*print_dir(t_minishell *minishell)
+char	*print_dir(t_ms *ms)
 {
 	char	*cwd;
 	char	*aux;
@@ -20,8 +20,8 @@ char	*print_dir(t_minishell *minishell)
 	cwd = ft_calloc(256, sizeof(char));
 	cwd = getcwd(cwd, 256);
 	aux = ft_strjoin("Minishell:", cwd);
-	minishell->prompt.prompt = ft_strjoin(aux, "$ ");
+	ms->prompt.prompt = ft_strjoin(aux, "$ ");
 	free(aux);
 	free(cwd);
-	return (minishell->prompt.prompt);
+	return (ms->prompt.prompt);
 }
