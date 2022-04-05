9187500d388712c8b3f366ea0061d5f19165f237
KrebsCoder <lucasklopess@gmail.com>
2022-04-05 00:21:16 -0300

    fix: now the tokenizer works properly

--- lexer/tokenizer.c
+++ lexer/tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 21:30:59 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/04/02 19:12:59 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/04/04 23:45:18 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,30 @@
 
 void	tokenizer(t_ms *ms)
 {
-	int	i;
-	int	c;
+	size_t	i;
+	int		c;
 
 	i = 0;
-
-	while (ms->lexer.line[i])
+	while (i < ft_strlen(ms->lexer.line))
 	{
 		if (ms->lexer.line[i] == SINGLE_QUOTE)
-		{
 			i = special_case(ms, SINGLE_QUOTE, i);
-		}
 		else if (ms->lexer.line[i] == DOUBLE_QUOTE)
-		{
 			i = special_case(ms, DOUBLE_QUOTE, i); 
-		}
-		else if (ms->lexer.line[i] == SPACE)
+		else if (ms->lexer.line[i] == FT_SPACE)
+		{
+			i++;
 			continue ;
+		}
 		else
 		{
 			c = i;
-			while(ms->lexer.line[i] != SPACE)
+			while(ms->lexer.line[i] != FT_SPACE && ms->lexer.line[i])
 				i++;
 			ms->lexer.tokens[ms->lexer.i_token] = ft_substr(ms->lexer.line, c, i - c);
 			ms->lexer.i_token++;
 		}
+		printf("%s\n", ms->lexer.tokens[ms->lexer.i_token - 1]);
 		i++;
 	}
 }
