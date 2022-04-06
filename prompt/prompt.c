9e25a0dfb03eb570981f890ae32d79c821e28a14
KrebsCoder <lucasklopess@gmail.com>
2022-04-06 03:43:50 -0300

    modify: removed signals related functions and added a new simplified one

--- prompt/prompt.c
+++ prompt/prompt.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   prompt.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 22:39:38 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/04/05 22:23:57 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/04/06 02:20:50 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,14 @@ void	clear(void)
 	printf("\033[H\033[J");
 }
 
-static void sigint_handler(int number)
-{
-	(void)number;
-	write(2, "\n", 1);
-	close(STDIN_FILENO);
-	open(STDIN_FILENO, O_RDWR, 0777);
-	// fprintf(stderr, "Ctrl-C (%d)\n", signum);
-}
-
 void	prompt(t_ms *ms)
 {
 	char				*line;
-	struct sigaction	sig;
 
 	clear();
-	sig.sa_handler = sigint_handler;
-	sigemptyset(&sig.sa_mask);
-	sigaction(SIGINT, &sig, NULL);
 	while(1)
 	{
+		ft_signal(ms);
 		line = readline(print_dir(ms));
 		if (!line)
 		{
