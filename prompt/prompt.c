e0523c561b395cf16d6ea3d8713cc13ec999f2dc
gabriela <gabriela@sertori.com.br>
2022-04-05 22:53:29 -0300

    add: start handlering signals

--- prompt/prompt.c
+++ prompt/prompt.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   prompt.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 22:39:38 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/04/04 22:03:53 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/04/05 22:23:57 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,33 @@ void	clear(void)
 	printf("\033[H\033[J");
 }
 
+static void sigint_handler(int number)
+{
+	(void)number;
+	write(2, "\n", 1);
+	close(STDIN_FILENO);
+	open(STDIN_FILENO, O_RDWR, 0777);
+	// fprintf(stderr, "Ctrl-C (%d)\n", signum);
+}
+
 void	prompt(t_ms *ms)
 {
-	char *line;
+	char				*line;
+	struct sigaction	sig;
 
 	clear();
+	sig.sa_handler = sigint_handler;
+	sigemptyset(&sig.sa_mask);
+	sigaction(SIGINT, &sig, NULL);
 	while(1)
 	{
 		line = readline(print_dir(ms));
-		if (ft_strlen(line) != 0)
+		if (!line)
+		{
+			printf("\n");
+			exit(1);
+		}
+		else if (ft_strlen(line) != 0)
 		{
 			history(line);
 			ms->lexer.line = ft_strdup(line);
