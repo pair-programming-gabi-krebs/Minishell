5d81c20a53e8850377f8d91390b581ca99923cf5
gabriela <gabriela@sertori.com.br>
2022-04-06 23:22:39 -0300

    add: ft_init initialize lexer_tokens[0] and prev_line

--- prompt/ft_init.c
+++ prompt/ft_init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_init.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 22:32:14 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/04/06 20:23:28 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/04/06 22:59:06 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,9 @@ void	ft_init(t_ms *ms, int argc, char *argv[], char *envp[])
 	ms->init.envp = envp;
 	ms->lexer.i_token = 0;
 	ms->lexer.tokens = malloc(2048 * sizeof(char **));
+	ms->lexer.tokens[0] = NULL;
 	ms->prompt.line = NULL;
 	ms->lexer.line = NULL;
+	ms->prompt.prev_line = NULL;
 	ft_signal(ms);
 }
