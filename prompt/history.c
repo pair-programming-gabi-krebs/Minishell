fcdd5a7e9096cec0a005763b22a91d9229af5e74
gabriela <gabriela@sertori.com.br>
2022-04-06 23:23:48 -0300

    modify: new logic with structe data, removed static char previous_line

--- prompt/history.c
+++ prompt/history.c
@@ -6,23 +6,23 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 23:22:05 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/03/30 23:17:17 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/04/06 23:06:52 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	history(char *line)
+void	history(t_ms *ms, char *line)
 {
-	static char	*previous_line = NULL;
-
-	if (!previous_line)
+	if (!ms->prompt.prev_line)
 	{
-		previous_line = ft_strdup(line);
 		add_history(line);
 	}
-	if (ft_strncmp(previous_line, line, ft_strlen(line))
-		|| ft_strncmp(line, previous_line, ft_strlen(previous_line)))
-		add_history(line);
-	previous_line = line;
+	else if (ft_strncmp(ms->prompt.prev_line, line, ft_strlen(line))
+		|| ft_strncmp(line, ms->prompt.prev_line, ft_strlen(ms->prompt.prev_line)))
+		{
+			add_history(line);
+		}
+	free(ms->prompt.prev_line);
+	ms->prompt.prev_line = ft_strdup(line);
 }
