43025ef89af42dc1c5acfb81cce7df595a1d7770
gabriela <gabriela@sertori.com.br>
2022-04-07 23:12:02 -0300

    modify: colors! \o/

--- prompt/print_dir.c
+++ prompt/print_dir.c
@@ -6,7 +6,7 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 22:45:16 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/04/01 22:56:11 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/04/07 21:43:45 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*print_dir(t_ms *ms)
 
 	cwd = ft_calloc(256, sizeof(char));
 	cwd = getcwd(cwd, 256);
-	aux = ft_strjoin("Minishell:", cwd);
+	aux = ft_strjoin("\001\033[1;35m\002Minishell:\001\033[0m\002", cwd);
 	ms->prompt.prompt = ft_strjoin(aux, "$ ");
 	free(aux);
 	free(cwd);
