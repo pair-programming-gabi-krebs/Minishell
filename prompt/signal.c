6606f2effa1c263a07e15baba0fdd6cd977fe95a
gabriela <gabriela@sertori.com.br>
2022-04-07 23:10:03 -0300

    fix: add SA_RESTART to flags, and moved sigaction to signal

--- prompt/signal.c
+++ prompt/signal.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signal.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 01:56:57 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/04/06 19:49:28 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/04/07 21:24:53 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,20 @@
 
 void	sigint_handler(int number)
 {
-	(void)number;
+	if (number == SIGQUIT)
+		return ;
 	write(2, "\n", 1);
 	rl_on_new_line();
-	//rl_replace_line("", 0);
+	rl_replace_line("", 0);
 	rl_redisplay();
 }
 
 void	ft_signal(t_ms *ms)
 {
 	ms->signal.sig.sa_handler = &sigint_handler;
+	ms->signal.sig.sa_flags = SA_RESTART;
 	sigemptyset(&ms->signal.sig.sa_mask);
 	sigaddset(&ms->signal.sig.sa_mask, SIGQUIT);
+	sigaction(SIGINT, &ms->signal.sig, NULL);
+	sigaction(SIGQUIT, &ms->signal.sig, NULL);
 }
