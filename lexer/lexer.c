dd15b86b56458c86ae69297e1ff4c71a60f4eb85
KrebsCoder <lucasklopess@gmail.com>
2022-04-12 22:23:15 -0300

    modify: removed unnecessary commentary

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lexer.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 21:26:23 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/04/01 22:56:58 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/04/12 21:53:51 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,4 @@
 void	lexer(t_ms *ms)
 {
 	tokenizer(ms);
-	// handle_redirect(ms);
 }
