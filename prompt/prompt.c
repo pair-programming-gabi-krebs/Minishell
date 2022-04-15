396fef32b73c8982044f16b8ca1199016ca84c43
gabriela <gabriela@sertori.com.br>
2022-04-15 19:03:49 -0300

    modify: refactoring prompt

--- prompt/prompt.c
+++ prompt/prompt.c
@@ -6,7 +6,7 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 22:39:38 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/04/12 22:53:45 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/04/15 18:54:44 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,38 +23,11 @@ void	prompt(t_ms *ms)
 	while (1)
 	{
 		ms->prompt.line = readline(print_dir(ms));
-		if(ms->prompt.line)
-		{
-			history(ms, ms->prompt.line);
-			pre_token(ms);
-		}
-		free(ms->prompt.prompt); // choose a better place to be
-		if (!ms->prompt.line)
-		{
-			printf("\n");
-			free_matrix(ms->lexer.tokens);
-			free(ms->prompt.prev_line);
-			ft_free(ms);
-			exit(1);
-		}
-		else if (!ft_strncmp(ms->prompt.line, "exit", 5))
-		{
-			free_matrix(ms->lexer.tokens);
-			free(ms->prompt.prev_line);
-			ft_free(ms);
-			exit(1);
-		}
+		if (!ms->prompt.line || !ft_strncmp(ms->prompt.line, "exit", 5))
+			ft_exit(ms);
 		else if (ft_strlen(ms->prompt.line) != 0)
-		{
-			ms->lexer.line = ft_strdup(ms->prompt.line);
-			lexer(ms);
-			//parser(ms);
-			ft_reset(ms);
-		}
+			valid_input(ms);
 		else if (ft_strlen(ms->prompt.line) == 0)
-			free(ms->prompt.line);
+			ft_free(ms);
 	}
 }
-
-// lc -la -> analise sintatica (por token)
-// lc -la wc -l  -> analise semantica (por linha digitada)
