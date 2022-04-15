e8605ea181f355c79b7c81b657e312c005b2ce8b
gabriela <gabriela@sertori.com.br>
2022-04-15 19:04:39 -0300

    add: new funtion exit and add more lines to be freed in ft_free

--- prompt/ft_exit.c
+++ prompt/ft_exit.c
@@ -1,35 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_free.c                                          :+:      :+:    :+:   */
+/*   ft_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/06 21:20:31 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/04/15 18:59:33 by gcosta-d         ###   ########.fr       */
+/*   Created: 2022/04/15 18:34:24 by gcosta-d          #+#    #+#             */
+/*   Updated: 2022/04/15 18:49:37 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	ft_free(t_ms *ms)
+void	ft_exit(t_ms *ms)
 {
-	if (ms->lexer.line)
-		free(ms->lexer.line);
-	free(ms->prompt.prompt);
-	free(ms->prompt.line);
-}
-
-void	free_matrix(char **matrix)
-{
-	int	i;
-
-	i = 0;
-	while (matrix[i])
-	{
-		free(matrix[i]);
-		i++;
-	}
-	free(matrix);
-	matrix = NULL;
+	printf("\n");
+	free_matrix(ms->lexer.tokens);
+	free(ms->prompt.prev_line);
+	ft_free(ms);
+	exit(1);
 }
