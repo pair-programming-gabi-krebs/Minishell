b4c4e7d8f7a95e2b93007c6d4ac4fdd39b545a15
KrebsCoder <lucasklopess@gmail.com>
2022-04-18 20:30:39 -0300

    add: new functions to pre_token

--- lexer/pre_token.c
+++ lexer/pre_token.c
@@ -3,14 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   pre_token.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 23:09:18 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/04/15 22:36:04 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/04/18 20:27:58 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+static int	quote_length(char *string, int i);
+static int	chars_length(char *string, int i);
+static int	is_special(t_ms *ms, char c);
+static int	verify_next_bytes(char *string, int i);
 
 void	pre_token(t_ms *ms)
 {
@@ -22,27 +27,26 @@ void	pre_token(t_ms *ms)
 	i = 0;
 	j = 0;
 	len = 0;
+	matrix = malloc(10 * sizeof(char **));
 	while(ms->prompt.line[i])
 	{
 		if (is_special(ms, ms->prompt.line[i]))
 		{
 			matrix[j] = ft_substr(ms->prompt.line, i - len, len);
+			printf("matrix[%d] %s\n", j, matrix[j]);
 			j++;
-			len = funcao();
+			len = verify_next_bytes(ms->prompt.line, i);
+			i += len;
 			if (len != 0)
 			{
 				matrix[j] = ft_substr(ms->prompt.line, i - len, len);
+				printf("matrix[%d] %s\n", j, matrix[j]);
 				j++;
 			}
 		}
 		len++;
 		i++;
 	}
-}
-
-static int	verify_next_bytes(t_ms *ms)
-{
-	
 }
 // banana <<<da"terra <"<ruim
 // j = 1;
@@ -66,6 +70,47 @@ int	is_special(t_ms *ms, char c)
 	}
 	return (0);
 }
+
+static int	verify_next_bytes(char *string, int i)
+{
+	int	j;
+
+	j = 0;
+	if (string[i] == SINGLE_QUOTE || string[i] == DOUBLE_QUOTE)
+		j = quote_length(string, i);
+	else if (string[i] == '<' || string[i] == '>' || string[i] == '|')
+		j = chars_length(string, i);
+	return (j);
+}
+
+static int	quote_length(char *string, int i)
+{
+	int		len;
+	char	character;
+
+	character = string[i];
+	len = i;
+	while (string[i + 1] != character)
+		i++;
+	return (i - len + 1);
+}
+
+static int	chars_length(char *string, int i)
+{
+	int		len;
+	char	character;
+
+	character = string[i];
+	len = i;
+	while (string[i + 1] == character)
+		i++;
+/* 	printf("i: %d\n", i);
+	printf("len: %d\n", len);
+	printf("i - len + 1: %d\n", (i - len + 1)); */
+	return (i - len + 1);
+}
+
+
 /*
 banana<<dsadsa<oi|"hoje<<<leia"
 
