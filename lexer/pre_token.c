c35dd13c9d02db22f03829c62880b586e9d41454
KrebsCoder <lucasklopess@gmail.com>
2022-04-22 20:15:03 -0300

    remove: unnecessary commentaries

--- lexer/pre_token.c
+++ lexer/pre_token.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pre_token.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 23:09:18 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/04/22 20:14:08 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/04/22 20:14:51 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,74 +98,3 @@ static int	handle_quote(t_ms *ms, int i, char chr)
 	printf("i after while %d\n", i);
 	return (i);
 }
-/*
-banana<<dsadsa<oi|"hoje<<<leia"
-
-specials = [<, >, ", ', |]
-count = 0;
-matrix[0] = bananaSPACE
-matrix[1] = <<SPACE
-matrix[2] = dsadsaSPACE
-matrix[3] = <SPACE
-
-
-1. conta quantos '<' tem
-2. verifica se o proximo é igual a ele
-3. verifica se o próximo do próximo tbm é igual
-
-se o proximo dps do delimitador (<) == ao delimitador, aciona a flag
-
-banana < dadsa
-1- banana
-2- <
-3- dsadsa
-
-achou o delimitador? Achou, verifica a flag, se for verdadeira, adiciona mais um delimitador
-
-
-banana << ddasd
-
-1- banana
-2- <<
-3- dsadsaasd
-*/
-
-// static void	normalize(t_ms *ms, char character, char *str);
-
-// void	pre_token(t_ms *ms)
-// {
-// 	if (ft_strchr(ms->prompt.line, '|'))
-// 		normalize(ms, '|', " | ");
-// 	if (ft_strchr(ms->prompt.line, '<'))
-// 		normalize(ms, '<', " < ");
-// 	if (ft_strchr(ms->prompt.line, '>'))
-// 		normalize(ms, '>', " > ");
-// }
-
-// static void	normalize(t_ms *ms, char character, char *str)
-// {
-// 	char	**split;
-// 	char	*prev;
-// 	char	*aux;
-// 	int		i;
-
-// 	split = ft_split(ms->prompt.line, character);
-// 	prev = ft_strdup(split[0]);
-// 	i = 1;
-// 	while (i < ft_mtxlen(split))
-// 	{
-// 		aux = ft_strjoin(prev, str);
-// 		free(prev);
-// 		prev = ft_strdup(aux);
-// 		free(aux);
-// 		aux = ft_strjoin(prev, split[i]);
-// 		free(prev);
-// 		prev = ft_strdup(aux);
-// 		free(aux);
-// 		i++;
-// 	}
-// 	free(ms->prompt.line);
-// 	ms->prompt.line = ft_strdup(prev);
-// 	free_matrix(split);
-// 	free(prev);
-// }
