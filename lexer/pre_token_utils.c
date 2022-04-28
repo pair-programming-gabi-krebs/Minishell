d40beae5075fcc1260b0f47279c50ecb0bc2e974
KrebsCoder <lucasklopess@gmail.com>
2022-04-27 23:18:41 -0300

    removed: old and unitialized functions

--- lexer/pre_token_utils.c
+++ lexer/pre_token_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pre_token_utils.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 20:17:13 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/04/26 22:59:02 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/04/27 22:30:42 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,45 +24,4 @@ int	is_special(t_ms *ms, char c)
 		i++;
 	}
 	return (0);
-}
-
-void make_substr_and_increment(t_ms *ms, int i, int len)
-{
-	// if (ms->prompt.line[i] == ' ')
-	// {
-	// 	printf("to aqui 2\n\n");
-	// 	return ;
-	// }
-	ms->tk.mtx[ms->tk.j] = ft_substr(ms->prompt.line, ms->tk.k, len);
-	printf("ms->tk.mtx[%d] = %s\n\n", ms->tk.j, ms->tk.mtx[ms->tk.j]);
-	ms->tk.j++;
-	ms->tk.k = i;
-}
-
-int	handle_next_special_bytes(t_ms *ms, int i)
-{
-	char	chr;
-
-	ms->tk.len = 0;
-	chr = ms->prompt.line[i];
-	while (is_special(ms, ms->prompt.line[i]) && ms->prompt.line[i] == chr)
-	{
-		ms->tk.len++;
-		i++;
-	}
-	return (i);
-}
-
-int	handle_quote(t_ms *ms, int i, char chr)
-{
-	ms->tk.len = 1;
-	i++;
-	while (ms->prompt.line[i] != chr)
-	{
-		i++;
-		ms->tk.len++;
-	}
-	i++;
-	ms->tk.len++;
-	return (i);
 }
