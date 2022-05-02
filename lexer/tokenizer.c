7f737c9759bca73e1986a6407b02b80d53adeba2
KrebsCoder <lucasklopess@gmail.com>
2022-05-02 20:18:21 -0300

    removed: unnecessary comments

--- lexer/tokenizer.c
+++ lexer/tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 21:30:59 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/04/22 18:22:40 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/04/30 23:05:15 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ void	tokenizer(t_ms *ms)
 		else
 		{
 			c = i;
-			while(ms->lexer.line[i] != FT_SPACE && ms->lexer.line[i])
+			while (ms->lexer.line[i] != FT_SPACE && ms->lexer.line[i])
 				i++;
 			ms->lexer.tokens[ms->lexer.i_token] = ft_substr(ms->lexer.line, \
 				c, i - c);
@@ -40,72 +40,5 @@ void	tokenizer(t_ms *ms)
 		}
 		i++;
 	}
-	for(int i = 0; i < ms->lexer.i_token; i++)
-		printf("%s\n", ms->lexer.tokens[i]);
 	ms->lexer.tokens[ms->lexer.i_token] = NULL;
 }
-
-
-/* V2.0 tokenizer
- 	if (strichar(ms->lexer.line, 0, SINGLE_QUOTE) == -1
-		&& strichar(ms->lexer.line, 0, DOUBLE_QUOTE) == -1)
-		common_case(ms);
-	else
-		special_case(ms);
-
-*/
-
-/* V1.0 tokenizer
-void	tokenizer(t_minishell *minishell)
-{
-	int	i;
-
-	i = 0;
-	while (minishell->lexer.line[i])
-	{
-		if (minishell->lexer.line[i] == '<' || minishell->lexer.line[i] == '>')
-			handle_redirect(minishell);
-		else if (minishell->lexer.line[i] == '$')
-			handle_vars(minishell);
-		else if (minishell->lexer.line[i] == '<'
-			&& minishell->lexer.line[i + 1] == '<')
-			handle_delimiter(minishell);
-		else if (minishell->lexer.line[i] == '>'
-			&& minishell->lexer.line[i + 1] == '>')
-			handle_append(minishell);
-		else if (minishell->lexer.line[i] == '\''
-			|| minishell->lexer.line[i] == '"')
-			handle_quotes(minishell);
-		else if (ft_isalpha(minishell->lexer.line[i]))
-			handle_cmds(minishell);
-		i++;
-	}
-}
-*/
-
-/* [ COMANDO ] echo "e<e"     <   README.md
- * 	| depois de um comando, sempre vem uma option se começar com
- * 	| caracter alfabético, - ou aspas.
- * 	|
- * [ REDIRECT ] <infile  OU   > outfile
- * 	| depois de um caracter de redirect, sempre vem o nome do arquivo
- * 	|
- * [ DELIMITER ] << LIMITER
- * 	| depois de um caracter de delimitação sempre vem a palavra delimitadora
- * 	|
- * [ APPEND ] >>
- * 	|
- * [ ASPAS ] "l"s    OU   'l's
- * 	| depois de aspas quase sempre vem algo para ser tratado como string
- * 	|
- * [ $ ] $ARGS
- * 	|
- * [ SPACE ]           ls -la
- * 	| pode receber qualquer coisa depois dele
- *
- *
- *
- * <   README.md echo               "e<e"
- * REDIRECT_IN CMD OPTION
- * <README.md echo "e<e"
-*/
