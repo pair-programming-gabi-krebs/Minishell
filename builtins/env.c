6cde585ebd0da8e55bc55989e926e1fe6bc3a012
KrebsCoder <lucasklopess@gmail.com>
2022-05-05 16:38:57 -0300

    modify: now env works with cpy_envp

--- builtins/env.c
+++ builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 22:43:45 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/05/03 23:18:45 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/05/05 14:39:35 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ void	env(t_ms *ms)
 	int	i;
 
 	i = 0;
-	while (ms->init.envp[i])
+	while (ms->prompt.cpy_envp[i])
 	{
-		printf("%s\n", ms->init.envp[i]);
+		printf("%s\n", ms->prompt.cpy_envp[i]);
 		i++;
 	}
 }
