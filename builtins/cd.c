75b71cd3a3ec11b8ef2d3c66e66881cb9559078c
gabriela <gabriela@sertori.com.br>
2022-05-05 20:02:50 -0300

    fix and modify: cd

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,28 +6,68 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 21:13:41 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/05/04 21:44:53 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/05/05 00:05:05 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+static void	update_pwd_and_oldpwd(t_ms *ms, char *current_pwd);
+
 int	cd(t_ms *ms)
 {
+	/* mock da line do prompt */
 	char	**line_mtx;
-	char	*current_wd;
-	int		return_value;
-
 	line_mtx = ft_split(ms->tk.line, ' ');
-	return_value = chdir(line_mtx[1]);
-	current_wd = ft_calloc(256, sizeof(char));
-	current_wd = getcwd(current_wd, 256);
-	printf("PWD: %s\n", current_wd);
-	printf("OLDPWD: %s\n", ms->prompt.cwd);
+	/*						  */
 
-	return (return_value);
+	char	*current_pwd;
+	char	*aux;
+	int		return_chdir;
+
+	if (!ft_strncmp(line_mtx[1], "~", ft_strlen(line_mtx[1]))
+		|| !ft_strncmp(line_mtx[1], "~/", ft_strlen(line_mtx[1])))
+		line_mtx[1] = getenv("HOME");
+	else if (!ft_strncmp(line_mtx[1], "~/", 2))
+	{
+		printf("to aqui\n");
+		aux = ft_strtrim(ft_strdup(line_mtx[1]), "~");
+		free(line_mtx[1]);
+		line_mtx[1] = ft_strjoin(getenv("HOME"), aux);
+		printf("LINE: %s\n", line_mtx[1]);
+		free(aux);
+	}
+	return_chdir = chdir(line_mtx[1]);
+	current_pwd = ft_calloc(256, sizeof(char));
+	current_pwd = getcwd(current_pwd, 256);
+	update_pwd_and_oldpwd(ms, current_pwd);
+	return (return_chdir);
 }
 
-/*
-	Confere se só existe 2 argumentos: cd e o path
-*/
+static void	update_pwd_and_oldpwd(t_ms *ms, char *current_pwd)
+{
+	int		mtx_len;
+	int		has_oldpwd;
+	int		i;
+
+	mtx_len = ft_mtxlen(ms->init.envp);
+	has_oldpwd = 0;
+	i = 0;
+	while (i < mtx_len)
+	{
+		if (!ft_strncmp(ms->init.envp[i], "PWD=", 4))
+			ms->init.envp[i] = ft_strjoin("PWD=", current_pwd);
+		else if (!ft_strncmp(ms->init.envp[i], "OLDPWD=", 7))
+		{
+			ms->init.envp[i] = ft_strjoin("OLDPWD=", ms->prompt.cwd);
+			has_oldpwd = 1;
+		}
+		i++;
+	}
+	if (has_oldpwd == 0)
+	{
+		ms->init.envp[i] = ft_strjoin("OLDPWD=", ms->prompt.cwd);
+		ms->init.envp[i+1] = NULL;
+	}
+	free(current_pwd);
+}
--- prompt/valid_input.c
+++ prompt/valid_input.c
@@ -6,7 +6,7 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 18:47:34 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/05/03 21:37:53 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/05/04 23:40:50 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ void	valid_input(t_ms *ms)
 	check_special_tokens(ms);
 	//ms->lexer.line = ft_strdup(ms->tk.line);
 	cd(ms);
+	//env(ms);
 	//lexer(ms);
 	//parser(ms);
 	ft_reset(ms);
