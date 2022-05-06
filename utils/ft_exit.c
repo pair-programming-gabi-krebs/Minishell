fed69a510edc3ce6609610eab5fdd64c13ac279d
gabriela <gabriela@sertori.com.br>
2022-05-05 21:22:17 -0300

    fix: memory leak

--- utils/ft_exit.c
+++ utils/ft_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 18:34:24 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/04/28 22:12:54 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/05/05 20:14:09 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void	ft_exit(t_ms *ms)
 	free_matrix(ms->lexer.tokens);
 	free(ms->prompt.prev_line);
 	free(ms->prompt.special);
+	free_matrix(ms->prompt.cpy_envp);
 	ft_free(ms);
 	exit(1);
 }
