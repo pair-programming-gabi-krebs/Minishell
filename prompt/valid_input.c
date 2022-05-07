7d17903dc15481f2b2d99314311becf9a1e5662e
gabriela <gabriela@sertori.com.br>
2022-05-07 00:28:24 -0300

    add: export and env working fine

--- prompt/valid_input.c
+++ prompt/valid_input.c
@@ -6,7 +6,7 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 18:47:34 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/05/04 23:40:50 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/05/06 23:56:59 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,12 @@ void	valid_input(t_ms *ms)
 	pre_token(ms);
 	check_special_tokens(ms);
 	//ms->lexer.line = ft_strdup(ms->tk.line);
-	cd(ms);
+	//cd(ms);
+	export(ms, "OI=testinho");
 	//env(ms);
+	printf("\n\n\n");
+	unset(ms, "GJS_DEBUG_TOPICS");
+	env(ms);
 	//lexer(ms);
 	//parser(ms);
 	ft_reset(ms);
