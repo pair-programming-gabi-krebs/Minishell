84b9c908e72ee97d4b56852832cd49d55986a149
KrebsCoder <lucasklopess@gmail.com>
2022-05-19 19:54:41 -0300

    fix: now it doesn't segfault

--- pipeline/is_builtin.c
+++ pipeline/is_builtin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   is_builtin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 21:26:20 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/05/16 21:58:42 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/05/17 23:38:12 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ int	is_builtin(t_ms *ms)
 	char	*cmd;
 
 	cmd = ms->cmds.command[0];
-	if (!ft_strncmp(cmd, "cd", ft_strlen(cmd))
-		|| !ft_strncmp(cmd, "echo", ft_strlen(cmd))
-		|| !ft_strncmp(cmd, "env", ft_strlen(cmd))
-		|| !ft_strncmp(cmd, "export", ft_strlen(cmd))
-		|| !ft_strncmp(cmd, "pwd", ft_strlen(cmd))
-		|| !ft_strncmp(cmd, "unset", ft_strlen(cmd)))
+	if ((cmd && !ft_strncmp(cmd, "cd", ft_strlen(cmd)))
+		|| (cmd && !ft_strncmp(cmd, "echo", ft_strlen(cmd)))
+		|| (cmd && !ft_strncmp(cmd, "env", ft_strlen(cmd)))
+		|| (cmd && !ft_strncmp(cmd, "export", ft_strlen(cmd)))
+		|| (cmd && !ft_strncmp(cmd, "pwd", ft_strlen(cmd)))
+		|| (cmd && !ft_strncmp(cmd, "unset", ft_strlen(cmd))))
 		return (1);
 	return (0);
 }
