02aba8fa24a4e0f892d228164420e04e69a60109
KrebsCoder <lucasklopess@gmail.com>
2022-05-19 19:53:54 -0300

    remove: build cmd from exec_cmd

--- pipeline/exec_commands.c
+++ pipeline/exec_commands.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 22:14:31 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/05/17 22:14:43 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/05/17 23:47:59 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 
 void	exec_commands(t_ms *ms, int i)
 {
-	build_cmd_table(ms);
 	ms->cmds.file_path = command_finder(ms);
 	if (ms->cmds.file_path == NULL)
 		ft_exit(ms);
 	resolve_dups(ms, i);
+/* 	for (int i = 0; i < 3; i++)
+		dprintf(2, "ms->cmds.command[%d]: %s\n", i, ms->cmds.command[i]); */
 	if (execve(ms->cmds.file_path, ms->cmds.command, ms->init.envp) == -1)
 		ft_exit(ms);
 }
