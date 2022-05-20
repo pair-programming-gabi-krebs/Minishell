5550d98524aef7989678e1237b06de85cfbb575d
KrebsCoder <lucasklopess@gmail.com>
2022-05-19 21:30:42 -0300

    add: dup42 and an open to a file for testing

--- pipeline/resolve_dups.c
+++ pipeline/resolve_dups.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 22:14:52 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/05/17 22:15:12 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/05/19 20:51:57 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,14 @@
 
 void	resolve_dups(t_ms *ms, int i)
 {
+	printf("ms->parser.pipes_qtn: %d\n", ms->parser.pipes_qtn);
 	if (i == ms->parser.pipes_qtn - 1)
-		return ;
-	else
 	{
-		dup2(ms->cmds.fd[1], STDOUT_FILENO);
-		close(ms->cmds.fd[1]);
+		int	tmp;
+		tmp = open("testDup", O_RDWR | O_CREAT | O_TRUNC, 0777);
+		printf("tmp: %d\n", tmp);
+		dup42(tmp, ms->cmds.fd[1]);
 	}
+	else
+		dup42(ms->cmds.fd[1], STDOUT_FILENO);
 }
