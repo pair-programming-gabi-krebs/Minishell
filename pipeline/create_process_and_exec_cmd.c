125772edf9ddd18cc31418a94edb69df41693db6
KrebsCoder <lucasklopess@gmail.com>
2022-05-19 21:30:06 -0300

    add: close to fd[0]

--- pipeline/create_process_and_exec_cmd.c
+++ pipeline/create_process_and_exec_cmd.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 22:24:14 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/05/17 22:25:17 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/05/19 20:45:21 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,6 @@ void	create_process_and_exec_cmd(t_ms *ms, int i)
 	{
 		waitpid(ms->cmds.pid, &ms->cmds.exit_status, 0);
 		close(ms->cmds.fd[1]);
+		close(ms->cmds.fd[0]);
 	}
 }
