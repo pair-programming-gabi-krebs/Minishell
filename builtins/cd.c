545dde1dde683fcdb7728b0d1493961692242b52
gabriela <gabriela@sertori.com.br>
2022-05-24 23:20:25 -0300

    fix: builtins cd and echo

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 21:13:41 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/05/19 21:41:35 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/05/24 23:16:32 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,34 @@ static void	update_pwd_and_oldpwd(t_ms *ms, char *current_pwd);
 int	cd(t_ms *ms)
 {
 	char	*current_pwd;
-	char	*aux;
+	char	*aux1;
+	char	*aux2;
+	char	*path;
 	int		return_chdir;
 
-	if (!ft_strncmp(ms->cmds.command[1], "~", ft_strlen(ms->cmds.command[1]))
-		|| !ft_strncmp(ms->cmds.command[1], "~/", ft_strlen(ms->cmds.command[1])))
-		ms->cmds.command[1] = getenv("HOME");
-	else if (!ft_strncmp(ms->cmds.command[1], "~/", 2))
+	path = NULL;
+	if (!ms->cmds.command[1])
+		path = ft_strdup(getenv("HOME"));
+	else if (ms->cmds.command[1]
+		&& (!ft_strncmp(ms->cmds.command[1], "~", ft_strlen(ms->cmds.command[1]))
+		|| !ft_strncmp(ms->cmds.command[1], "~/", ft_strlen(ms->cmds.command[1]))))
+		path = ft_strdup(getenv("HOME"));
+	else if (ms->cmds.command[1] && !ft_strncmp(ms->cmds.command[1], "~/", 2))
 	{
-		//dprintf(2, "to aqui\n");
-		aux = ft_strtrim(ft_strdup(ms->cmds.command[1]), "~");
-		free(ms->cmds.command[1]);
-		ms->cmds.command[1] = ft_strjoin(getenv("HOME"), aux);
-		//printf("LINE: %s\n", ms->cmds.command[1]);
-		free(aux);
+		aux1 = ft_strdup(ms->cmds.command[1]);
+		aux2 = ft_strtrim(aux1, "~");
+		path = ft_strjoin(getenv("HOME"), aux2);
+		free(aux1);
+		free(aux2);
 	}
-	return_chdir = chdir(ms->cmds.command[1]);
+	else if (ms->cmds.command[1])
+		path = ft_strdup(ms->cmds.command[1]);
+	if (path)
+		return_chdir = chdir(path);
 	current_pwd = ft_calloc(256, sizeof(char));
 	current_pwd = getcwd(current_pwd, 256);
 	update_pwd_and_oldpwd(ms, current_pwd);
-	//free_matrix(ms->cmds.command);
+	free(path);
 	return (return_chdir);
 }
 
@@ -61,6 +69,12 @@ static void	update_pwd_and_oldpwd(t_ms *ms, char *current_pwd)
 			has_oldpwd = 1;
 		}
 		node = node->next;
+		if (node->next == NULL && !ft_strncmp(node->content, "OLDPWD=", 7))
+		{
+			free(node->content);
+			node->content = ft_strjoin("OLDPWD=", ms->prompt.cwd);
+			has_oldpwd = 1;
+		}
 	}
 	if (has_oldpwd == 0)
 	{
--- builtins/echo.c
+++ builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 23:08:19 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/05/12 20:45:25 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/05/24 21:31:21 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,35 +18,15 @@ void	echo(t_ms *ms)
 	int	i;
 
 	has_nl = 1;
-	if (!ft_strncmp(ms->lexer.tokens[1], "-n", ft_strlen(ms->lexer.tokens[1])))
+	if (!ft_strncmp(ms->cmds.command[1], "-n", ft_strlen(ms->cmds.command[1])))
 		has_nl = 0;
-	i = 0;
-	while (ms->lexer.tokens[++i] && !is_special(ms, ms->lexer.tokens[i][0]))
+	i = 1;
+	while (ms->cmds.command[i])
 	{
 		if (has_nl)
-			printf("%s\n", ms->lexer.tokens[i]);
-		else
-			printf("%s", ms->lexer.tokens[i + 1]);
+			printf("%s\n", ms->cmds.command[i]);
+		else if (ms->cmds.command[i + 1])
+			printf("%s", ms->cmds.command[i + 1]);
+		i++;
 	}
 }
-
-// echo teste "teste"
-
-// >outfile echo "teste"| tr ' ' a
-
-// >
-// outfile
-// echo
-// teste|
-// tr
-// ' '
-// a
-
-// "l"s
-
-/*
-	[x] Verificar se há -n
-	[ ] Verificar se há $
-	[ ] Verificar se há single quote
-	[ ] Verificar se há double quote
-*/
