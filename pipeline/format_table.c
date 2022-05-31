111b00f52896b83ed71f1ac8b2cf80d2e10b72fd
KrebsCoder <lucasklopess@gmail.com>
2022-05-31 14:36:59 -0300

    refactor: changed whole struct on format table and added new functions

--- pipeline/format_table.c
+++ pipeline/format_table.c
@@ -3,49 +3,95 @@
 /*                                                        :::      ::::::::   */
 /*   format_table.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 22:13:44 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/05/27 23:04:23 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/05/31 00:01:25 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-static void	get_redirect_name(t_ms *ms, char *redirect, int start);
+static void	get_redirect_name(t_ms *ms, char **redirect, int i, int counter);
+static void	check_for_redirects(t_ms *ms);
+static void	remove_redirect_from_end_of_str(t_ms *ms);
 
 void	format_table(t_ms *ms, int start, int end)
 {
-	int	j;
+	int	i;
 
-	j = 0;
+	i = 0;
 	while (start < end)
 	{
-		if (!ft_strncmp(ms->lexer.tokens[start], "<<", \
-			ft_strlen(ms->lexer.tokens[start])))
-			printf("<<\n");
-		else if (!ft_strncmp(ms->lexer.tokens[start], ">>", \
-			ft_strlen(ms->lexer.tokens[start])))
-			printf(">>\n");
-		else if (ms->lexer.tokens[start][0] == '>')
-			get_redirect_name(ms, ms->cmds.out, start);
-		else if (ms->lexer.tokens[start][0] == '<')
-			get_redirect_name(ms, ms->cmds.inf, start);
-		else
-		{
-			ms->cmds.command[j] = ft_strdup(ms->lexer.tokens[start]);
-			ms->cmds.command[j + 1] = NULL;
-			j++;
-		}
+		ms->cmds.command[i] = ft_strdup(ms->lexer.tokens[start]);
+		ms->cmds.command[i + 1] = NULL;
+		i++;
 		start++;
 	}
+	check_for_redirects(ms);
+	remove_redirect_from_end_of_str(ms);
 }
 
-static void	get_redirect_name(t_ms *ms, char *redirect, int start)
+static void	remove_redirect_from_end_of_str(t_ms *ms)
 {
-	if (redirect)
-		free(redirect);
-	if (ms->lexer.tokens[start + 1])
-		redirect = ft_strdup(ms->lexer.tokens[start + 1]);
-	printf("redirect: %s\n", redirect);
+	int	i;
+
+	i = 0;
+	while (ms->cmds.command[i])
+	{
+		if (ms->cmds.command[i][0] == '<' && ms->cmds.command[i + 1])
+		{
+			free(ms->cmds.command[i]);
+			free(ms->cmds.command[i + 1]);
+			ms->cmds.command[i] = NULL;
+			ms->cmds.command[i + 1] = NULL;
+		}
+		else if (ms->cmds.command[i][0] == '>' && ms->cmds.command[i + 1])
+		{
+			free(ms->cmds.command[i]);
+			free(ms->cmds.command[i + 1]);
+			ms->cmds.command[i] = NULL;
+			ms->cmds.command[i + 1] = NULL;
+		}
+		i++;
+	}
+	for (int q = 0; ms->cmds.command[q]; q++)
+		printf(" ms->cmds.command[%d]: %s\n", q, ms->cmds.command[q]);
+	
+}
+
+static void	check_for_redirects(t_ms *ms)
+{
+	int	i;
+	int	out_counter;
+	int	inf_counter;
+
+	out_counter = 0;
+	inf_counter = 0;
+	i = 0;
+	while (ms->cmds.command[i])
+	{
+		if (ms->cmds.command[i][0] == '<')
+		{
+			get_redirect_name(ms, ms->cmds.inf, i, inf_counter);
+			inf_counter++;
+		}
+		else if (ms->cmds.command[i][0] == '>')
+		{
+			get_redirect_name(ms, ms->cmds.out, i, out_counter);
+			out_counter++;
+		}
+		i++;
+	}
+/* 	for (int q = 0; ms->cmds.inf[q]; q++)
+		printf("ms->cmds.inf[%d]: %s\n", q, ms->cmds.inf[q]);
+	for (int k = 0; ms->cmds.out[k]; k++)
+		printf("ms->cmds.out[%d]: %s\n", k, ms->cmds.out[k]); */
+}
+
+static void	get_redirect_name(t_ms *ms, char **redirect, int i, int counter)
+{
+	if (ms->cmds.command[i + 1])
+		redirect[counter] = ft_strdup(ms->cmds.command[i + 1]);
+	redirect[counter + 1] = NULL;
 }
