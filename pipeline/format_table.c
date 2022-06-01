a1a8a37d6daef0f2c3487a4e91025cb17609d46e
gabriela <gabriela@sertori.com.br>
2022-06-01 00:31:27 -0300

    add: new functionality remove redirects from start string

--- pipeline/format_table.c
+++ pipeline/format_table.c
@@ -3,18 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   format_table.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 22:13:44 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/05/31 00:01:25 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/06/01 00:17:40 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
 static void	get_redirect_name(t_ms *ms, char **redirect, int i, int counter);
-static void	check_for_redirects(t_ms *ms);
+static int	check_for_redirects(t_ms *ms);
 static void	remove_redirect_from_end_of_str(t_ms *ms);
+static void	remove_redirect_from_start_of_str(t_ms *ms);
 
 void	format_table(t_ms *ms, int start, int end)
 {
@@ -28,8 +29,88 @@ void	format_table(t_ms *ms, int start, int end)
 		i++;
 		start++;
 	}
-	check_for_redirects(ms);
-	remove_redirect_from_end_of_str(ms);
+	if (check_for_redirects(ms))
+	{
+		remove_redirect_from_start_of_str(ms);
+		remove_redirect_from_end_of_str(ms);
+	}
+}
+
+static int	check_for_redirects(t_ms *ms)
+{
+	int	i;
+	int	out_counter;
+	int	inf_counter;
+
+	out_counter = 0;
+	inf_counter = 0;
+	i = 0;
+	while (ms->cmds.command[i])
+	{
+		if (ms->cmds.command[i][0] == '<')
+		{
+			get_redirect_name(ms, ms->cmds.inf, i, inf_counter);
+			inf_counter++;
+		}
+		else if (ms->cmds.command[i][0] == '>')
+		{
+			get_redirect_name(ms, ms->cmds.out, i, out_counter);
+			out_counter++;
+		}
+		i++;
+	}
+	if (inf_counter > 0 || out_counter > 0)
+		return (1);
+	return (0);
+}
+
+static void	get_redirect_name(t_ms *ms, char **redirect, int i, int counter)
+{
+	if (ms->cmds.command[i + 1])
+		redirect[counter] = ft_strdup(ms->cmds.command[i + 1]);
+	redirect[counter + 1] = NULL;
+}
+
+static void	remove_redirect_from_start_of_str(t_ms *ms)
+{
+	int		i;
+	int		last_redirect;
+	int		mtx_len;
+	char	**mtx_aux;
+	int		stop_count;
+
+	mtx_len = ft_mtxlen(ms->cmds.command);
+	last_redirect = -1;
+	i = 0;
+	if (ms->cmds.command[i][0] != '<' && ms->cmds.command[i][0] != '>')
+		return ;
+	stop_count = 0;
+	while (ms->cmds.command[i] && stop_count < 2)
+	{
+		if (ms->cmds.command[i][0] == '<' && ms->cmds.command[i + 1])
+		{
+			last_redirect = last_redirect + 2;
+			stop_count = 0;
+		}
+		else if (ms->cmds.command[i][0] == '>' && ms->cmds.command[i + 1])
+		{
+			last_redirect = last_redirect + 2;
+			stop_count = 0;
+		}
+		else
+			stop_count++;
+		i++;
+	}
+	if (last_redirect < i)
+	{
+		mtx_len = mtx_len - (last_redirect + 1);
+		mtx_aux = malloc((mtx_len + 1) * sizeof(char *));
+		copy_mtx(mtx_aux, ms->cmds.command, last_redirect + 1, mtx_len);
+		ms->cmds.command = malloc((mtx_len + 1) * sizeof(char *));
+		copy_mtx(ms->cmds.command, mtx_aux, 0, mtx_len);
+	}
+	for (int k = 0; ms->cmds.command[k]; k++)
+		printf("command[%d] %s\n", k, ms->cmds.command[k]);
 }
 
 static void	remove_redirect_from_end_of_str(t_ms *ms)
@@ -55,43 +136,4 @@ static void	remove_redirect_from_end_of_str(t_ms *ms)
 		}
 		i++;
 	}
-	for (int q = 0; ms->cmds.command[q]; q++)
-		printf(" ms->cmds.command[%d]: %s\n", q, ms->cmds.command[q]);
-	
-}
-
-static void	check_for_redirects(t_ms *ms)
-{
-	int	i;
-	int	out_counter;
-	int	inf_counter;
-
-	out_counter = 0;
-	inf_counter = 0;
-	i = 0;
-	while (ms->cmds.command[i])
-	{
-		if (ms->cmds.command[i][0] == '<')
-		{
-			get_redirect_name(ms, ms->cmds.inf, i, inf_counter);
-			inf_counter++;
-		}
-		else if (ms->cmds.command[i][0] == '>')
-		{
-			get_redirect_name(ms, ms->cmds.out, i, out_counter);
-			out_counter++;
-		}
-		i++;
-	}
-/* 	for (int q = 0; ms->cmds.inf[q]; q++)
-		printf("ms->cmds.inf[%d]: %s\n", q, ms->cmds.inf[q]);
-	for (int k = 0; ms->cmds.out[k]; k++)
-		printf("ms->cmds.out[%d]: %s\n", k, ms->cmds.out[k]); */
-}
-
-static void	get_redirect_name(t_ms *ms, char **redirect, int i, int counter)
-{
-	if (ms->cmds.command[i + 1])
-		redirect[counter] = ft_strdup(ms->cmds.command[i + 1]);
-	redirect[counter + 1] = NULL;
 }
