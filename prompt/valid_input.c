ffaae4d055ba3bc8b6e5ad30ad0568b0fb11c540
KrebsCoder <lucasklopess@gmail.com>
2022-06-01 22:51:55 -0300

    remove: unnecessary comments

--- prompt/valid_input.c
+++ prompt/valid_input.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   valid_input.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 18:47:34 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/05/12 21:31:12 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/06/01 22:51:40 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,7 @@ void	valid_input(t_ms *ms)
 	check_special_tokens(ms);
 	ms->lexer.line = ft_strdup(ms->tk.line);
 	lexer(ms);
-	//parser(ms);
 	pipeline(ms);
-	//export(ms, "OI=testinho");
-	//unset(ms, "HOME");
-	//cd(ms);
-	//echo(ms);
-	//env(ms);
-	//pwd();
-	//parser(ms);
 	ft_reset(ms);
 }
 
