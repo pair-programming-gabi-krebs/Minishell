f4bf59d84ee1d577fc03a6e02061bc2692d03039
KrebsCoder <lucasklopess@gmail.com>
2022-06-01 22:48:40 -0300

    add: new free

--- utils/ft_exit.c
+++ utils/ft_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 18:34:24 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/05/31 22:36:40 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/06/01 22:44:43 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ void	ft_exit(t_ms *ms)
 	free(ms->prompt.special);
 	free_matrix(ms->cmds.out);
 	free_matrix(ms->cmds.inf);
+	if (ms->cmds.bin)
+		free_matrix(ms->cmds.bin);
 	ft_lstclear(ms->list, free);
 	ft_free(ms);
 	exit(1);
