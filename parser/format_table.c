aaacd7f0e61d1a0c05e8eb6125b6e07b77788e2f
KrebsCoder <lucasklopess@gmail.com>
2022-06-01 22:47:43 -0300

    modify: changed folders for format_table and parse_env files

--- parser/format_table.c
+++ parser/format_table.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   format_table.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 22:13:44 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/06/01 00:17:40 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/06/01 21:59:56 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,10 @@ void	format_table(t_ms *ms, int start, int end)
 		remove_redirect_from_start_of_str(ms);
 		remove_redirect_from_end_of_str(ms);
 	}
+	for (int k = 0; ms->cmds.inf[k]; k++)
+		printf("inf[%d] %s\n", k, ms->cmds.inf[k]);
+	for (int k = 0; ms->cmds.out[k]; k++)
+		printf("out[%d] %s\n", k, ms->cmds.out[k]);
 }
 
 static int	check_for_redirects(t_ms *ms)
@@ -70,6 +74,21 @@ static void	get_redirect_name(t_ms *ms, char **redirect, int i, int counter)
 		redirect[counter] = ft_strdup(ms->cmds.command[i + 1]);
 	redirect[counter + 1] = NULL;
 }
+
+/* static void	get_redirect_name(t_ms *ms, char **redirect, int i)
+{
+	int	j;
+
+	j = 0;
+	if (redirect[0])
+	{
+		while (redirect[j])
+			j++;
+	}
+	if (ms->cmds.command[i + 1])
+		redirect[j] = ft_strdup(ms->cmds.command[i + 1]);
+	redirect[j + 1] = NULL;
+} */
 
 static void	remove_redirect_from_start_of_str(t_ms *ms)
 {
@@ -109,8 +128,6 @@ static void	remove_redirect_from_start_of_str(t_ms *ms)
 		ms->cmds.command = malloc((mtx_len + 1) * sizeof(char *));
 		copy_mtx(ms->cmds.command, mtx_aux, 0, mtx_len);
 	}
-	for (int k = 0; ms->cmds.command[k]; k++)
-		printf("command[%d] %s\n", k, ms->cmds.command[k]);
 }
 
 static void	remove_redirect_from_end_of_str(t_ms *ms)
