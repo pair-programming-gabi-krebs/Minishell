6f8747ec7b37aa1cca7e2ade106a54c46985d81d
gabriela <gabriela@sertori.com.br>
2022-06-02 22:38:11 -0300

    fix: protecting mtx_len

--- libft/src/ft_mtxlen.c
+++ libft/src/ft_mtxlen.c
@@ -6,7 +6,7 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 20:54:46 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/04/11 21:45:10 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/06/02 22:24:00 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int	ft_mtxlen(char **mtx)
 {
 	int	i;
 
+	if (!mtx)
+		return (0);
 	i = 0;
 	while (mtx[i])
 		i++;
