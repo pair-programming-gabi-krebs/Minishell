063cc79db1203741ad0fe85ed237d7392fc02ea5
KrebsCoder <lucasklopess@gmail.com>
2022-06-06 20:56:02 -0300

    fix: minor bugs in command_finder function, now it's working properly

--- pipeline/command_finder.c
+++ pipeline/command_finder.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   command_finder.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 22:57:05 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/05/13 00:26:01 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/06/06 20:54:29 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,9 @@ char	*command_finder(t_ms *ms)
 	char	*cmd;
 
 	i = 0;
-	while (ms->init.envp[i])
+	while (ms->init.envp[i] && ms->cmds.bin[i])
 	{
 		path = ft_strjoin(ms->cmds.bin[i], "/");
-		free(ms->cmds.bin[i]);
 		cmd = ft_strjoin(path, ms->cmds.command[0]);
 		check = access(cmd, F_OK);
 		if (check == 0)
