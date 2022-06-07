c747c3a67a6726ef2d15827cfb039ed8b31243b7
KrebsCoder <lucasklopess@gmail.com>
2022-06-07 00:10:09 -0300

    add: new feature to echo, now it prints exit code

--- builtins/echo.c
+++ builtins/echo.c
@@ -3,19 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 23:08:19 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/05/27 22:31:41 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/06/06 23:59:51 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-static char	*search_var_on_env(t_ms *ms, char *str);
-static void	expand_var(t_ms *ms, char **echo, int j);
 static char	*join_strings(char **echo);
 static void	print_echo(t_ms *ms, int i);
+static void	handle_exit_code(t_ms *ms, int i);
 
 void	echo(t_ms *ms)
 {
@@ -29,6 +28,11 @@ void	echo(t_ms *ms)
 		printf("\n");
 		return ;
 	}
+	if (!ft_strncmp(ms->cmds.command[1], "$?", ft_strlen(ms->cmds.command[1])))
+		handle_exit_code(ms, 1);
+	else if (!ft_strncmp(ms->cmds.command[2], "$?", \
+		ft_strlen(ms->cmds.command[2])))
+		handle_exit_code(ms, 2);
 	if (!ft_strncmp(ms->cmds.command[1], "-n", ft_strlen(ms->cmds.command[1])))
 	{
 		has_nl = 0;
@@ -39,64 +43,30 @@ void	echo(t_ms *ms)
 		printf("\n");
 }
 
+static void	handle_exit_code(t_ms *ms, int i)
+{
+	char	*aux;
+
+	free(ms->cmds.command[i]);
+	aux = ft_itoa(ms->cmds.exit_status);
+	ms->cmds.command[i] = ft_strdup(aux);
+	free(aux);
+}
+
 static void	print_echo(t_ms *ms, int i)
 {
-	int		j;
 	char	**echo;
 	char	*str;
 
 	while (ms->cmds.command[i])
 	{
 		echo = ft_split(ms->cmds.command[i], ' ');
-		j = 0;
-		while (echo[j])
-		{
-			if (echo[j][0] == '$' && (ms->lexer.flag_quote == '\"'
-				|| !ms->lexer.flag_quote))
-				expand_var(ms, echo, j);
-			j++;
-		}
 		str = join_strings(echo);
 		printf("%s", str);
 		free(str);
 		free_matrix(echo);
 		i++;
 	}
-}
-
-static void	expand_var(t_ms *ms, char **echo, int j)
-{
-	char	*str;
-
-	str = ft_strdup(echo[j]);
-	free(echo[j]);
-	echo[j] = search_var_on_env(ms, (str + 1));
-	free(str);
-}
-
-static char	*search_var_on_env(t_ms *ms, char *str)
-{
-	t_list	*node;
-	char	*var;
-
-	node = *(ms->list);
-	while (node->next != NULL)
-	{
-		if (!ft_strncmp(node->content, str, ft_strlen(str)))
-		{
-			var = ft_strchr(node->content, '=');
-			var = ft_strdup(var + 1);
-			return (var);
-		}
-		node = node->next;
-		if (node->next == NULL && !ft_strncmp(node->content, \
-			str, ft_strlen(str)))
-		{
-			var = ft_strchr(node->content, '=');
-			return (ft_strdup(var + 1));
-		}
-	}
-	return (NULL);
 }
 
 static char	*join_strings(char **echo)
