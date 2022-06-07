ef9a18d391b65e21bc0cfb9520bc350b3799991d
KrebsCoder <lucasklopess@gmail.com>
2022-06-06 21:03:34 -0300

    fix: turning back on old change

--- pipeline/exec_commands.c
+++ pipeline/exec_commands.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 22:14:31 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/06/06 20:59:33 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/06/06 21:03:19 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,7 @@ void	exec_commands(t_ms *ms, int i)
 	ms->cmds.file_path = command_finder(ms);
 	if (!ms->cmds.file_path)
 		ft_exit(ms);
-	else
-	{
-		resolve_dups_outfile(ms, i);
-		if (execve(ms->cmds.file_path, ms->cmds.command, ms->init.envp) == -1)
-		ft_exit(ms);	
-	}
+	resolve_dups_outfile(ms, i);
+	if (execve(ms->cmds.file_path, ms->cmds.command, ms->init.envp) == -1)
+	ft_exit(ms);	
 }
