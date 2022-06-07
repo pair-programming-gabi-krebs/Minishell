ba2dda8d96e78a75d50b87fe283edde91043fb0e
KrebsCoder <lucasklopess@gmail.com>
2022-06-06 22:30:09 -0300

    add: new function that expand variables

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 21:26:23 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/05/19 20:23:01 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/06/06 22:27:54 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	lexer(t_ms *ms)
 {
+
 	tokenizer(ms);
 	remove_quotes(ms);
+	check_expand_var(ms);
 }
