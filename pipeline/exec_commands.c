81abd8276bc10640e6f6443c3d6a929090c428e4
KrebsCoder <lucasklopess@gmail.com>
2022-06-09 22:34:38 -0300

    remove: now resolve dups infile and outfile stays on other file

--- pipeline/exec_commands.c
+++ pipeline/exec_commands.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   exec_commands.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 22:14:31 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/06/08 19:02:38 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/06/09 21:37:04 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	exec_commands(t_ms *ms, int i)
+void	exec_commands(t_ms *ms)
 {
 	ms->cmds.file_path = command_finder(ms);
 	if (!ms->cmds.file_path)
@@ -20,8 +20,6 @@ void	exec_commands(t_ms *ms, int i)
 		printf("%s: command not found", ms->cmds.command[0]);
 		ft_exit(ms);
 	}
-	resolve_dups_infile(ms, i);
-	resolve_dups_outfile(ms, i);
 	if (execve(ms->cmds.file_path, ms->cmds.command, ms->init.envp) == -1)
 	{
 		dprintf(2, "exec fail\n");
