c7f3dfd6369cd28780f2a177042d903d2397710c
KrebsCoder <lucasklopess@gmail.com>
2022-06-09 22:36:00 -0300

    modify: added a new struct to if else

--- pipeline/resolve_dups_infile.c
+++ pipeline/resolve_dups_infile.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   resolve_dups_infile.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 19:24:48 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/06/08 18:50:58 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/06/09 21:58:53 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,13 @@ void	resolve_dups_infile(t_ms *ms, int i)
 		dprintf(2, "inf_fd |%d|\n", ms->cmds.inf_fd);
 		dup42(ms->cmds.inf_fd, STDIN_FILENO);
 	}
-	else if (i >= 0)
+	else if (i >= 0 && ms->cmds.aux_fd != -1)
 	{
 		dprintf(2, "fd[0] |%d|\n", ms->cmds.fd[0]);
-		dup42(ms->cmds.fd[0], STDIN_FILENO);
+		dup42(ms->cmds.aux_fd, STDIN_FILENO);
 	}
+	else if (i >= 0 && ms->cmds.aux_fd == -1)
+		dup42(ms->cmds.fd[0], STDIN_FILENO);
 }
 
 
