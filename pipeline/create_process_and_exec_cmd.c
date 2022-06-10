77dd8ad70502f9eb53963a498819cf2e732ca4f5
KrebsCoder <lucasklopess@gmail.com>
2022-06-09 22:33:16 -0300

    modify: refactor function to work properly on the pipeline

--- pipeline/create_process_and_exec_cmd.c
+++ pipeline/create_process_and_exec_cmd.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 22:24:14 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/06/09 20:41:15 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/06/09 22:18:19 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,13 @@
 
 void	create_process_and_exec_cmd(t_ms *ms, int i)
 {
+	resolve_dups_infile(ms, i);
+	resolve_dups_outfile(ms, i);
 	ms->cmds.pid = fork();
 	if (ms->cmds.pid == -1)
 		ft_exit(ms);
 	if (ms->cmds.pid == 0)
-		exec_commands(ms, i);
+		exec_commands(ms);
 	else
 	{
 		waitpid(ms->cmds.pid, &ms->cmds.exit_status, 0);
@@ -27,6 +29,6 @@ void	create_process_and_exec_cmd(t_ms *ms, int i)
 			ms->cmds.exit_status = WEXITSTATUS(ms->cmds.exit_status);
 		else if (WIFSIGNALED(ms->cmds.exit_status))
 			ms->cmds.exit_status = WTERMSIG(ms->cmds.exit_status) + 128;
-		close(ms->cmds.fd[1]);
+		ms->cmds.aux_fd = ms->cmds.fd[0];
 	}
 }
