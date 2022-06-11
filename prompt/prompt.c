9db08cd9d6723159fc3bebff14011add27838169
KrebsCoder <lucasklopess@gmail.com>
2022-06-10 23:23:59 -0300

    add: new parameter to ft_exit

--- prompt/prompt.c
+++ prompt/prompt.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 22:39:38 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/06/10 22:43:14 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/06/10 23:16:44 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	prompt(t_ms *ms)
 		if (!ms->prompt.line || !ft_strncmp(ms->prompt.line, "exit", 5))
 		{
 			write(1, "exit", 5);
-			ft_exit(ms);
+			ft_exit(ms, 0);
 		}
 		else if (ft_strlen(ms->prompt.line) != 0)
 			valid_input(ms);
