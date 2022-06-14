fec9d3ef68889e26a243bf58362089f97a893121
KrebsCoder <lucasklopess@gmail.com>
2022-06-14 02:03:27 -0300

    add: is_cmd function

--- pipeline/create_process_and_exec_cmd.c
+++ pipeline/create_process_and_exec_cmd.c
@@ -6,7 +6,11 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 22:24:14 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/06/14 02:00:07 by gcosta-d         ###   ########.fr       */
+<<<<<<< HEAD
+/*   Updated: 2022/06/13 23:16:14 by gcosta-d         ###   ########.fr       */
+=======
+/*   Updated: 2022/06/13 22:09:34 by lkrebs-l         ###   ########.fr       */
+>>>>>>> fe69493bd797e0e5077b04b396ca50fcadbf69c1
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +18,8 @@
 
 void	create_process_and_exec_cmd(t_ms *ms, int i)
 {
+	if (!is_cmd(ms))
+		return ;
 	resolve_dups_infile(ms, i);
 	resolve_dups_outfile(ms, i);
 	ms->cmds.pid = fork();
@@ -27,6 +33,7 @@ void	create_process_and_exec_cmd(t_ms *ms, int i)
 	}
 	else
 	{
+		// checkar por erro no waitpid **
 		waitpid(ms->cmds.pid, &ms->cmds.exit_status, 0);
 		if (WIFEXITED(ms->cmds.exit_status))
 			ms->cmds.exit_status = WEXITSTATUS(ms->cmds.exit_status);
@@ -34,4 +41,11 @@ void	create_process_and_exec_cmd(t_ms *ms, int i)
 			ms->cmds.exit_status = WTERMSIG(ms->cmds.exit_status) + 128;
 		ms->cmds.aux_fd = ms->cmds.fd[0];
 	}
+}
+
+static int	is_cmd(t_ms *ms)
+{
+	if (!ms->cmds.command[0])
+		return (0);
+	return (1);
 }
