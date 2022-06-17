e1ffd093568d94176e48d17ee00eeae690aca42b
gabriela <gabriela@sertori.com.br>
2022-06-16 21:44:00 -0300

    fix: here doc limiter

--- parser/here_doc.c
+++ parser/here_doc.c
@@ -6,7 +6,7 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 22:57:08 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/06/16 21:29:41 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/06/16 21:39:11 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,21 +23,20 @@ void	here_doc(t_ms *ms)
 	ft_init_sigaction(ms, handle_child_sig_int_here_doc, SIGINT);
 	ft_init_sigaction(ms, SIG_IGN, SIGQUIT);
 	temp_hdoc = open("hdoc_file", O_WRONLY | O_CREAT | O_APPEND, 0744);
-	line = readline("> ");
+	line = readline("");
 	while (1)
 	{
 		if (!line)
 		{
 			print_error_here_doc(ms, temp_hdoc);
-			//free(ms->cmds.limiter);
 			ft_exit(ms, 0);
 		}
 		if (!ft_strncmp(line, ms->cmds.limiter, ft_strlen(ms->cmds.limiter))
-			&& ft_strlen(ms->cmds.limiter) == ft_strlen(line) - 1)
+			&& ft_strlen(ms->cmds.limiter) == ft_strlen(line))
 			end_here_doc(ms, line, temp_hdoc);
 		write(temp_hdoc, line, ft_strlen(line));
 		free(line);
-		line = readline("> ");
+		line = readline("");
 	}
 }
 
@@ -56,6 +55,5 @@ static void	end_here_doc(t_ms *ms, char *line, int fd)
 	free(line);
 	close(fd);
 	ms->cmds.inf_fd = open("hdoc_file", O_RDONLY);
-	//free(ms->cmds.limiter);
 	ft_exit(ms, 0);
 }
