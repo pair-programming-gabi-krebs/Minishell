b76be148e38debe050898b34476fc26ff6d14f6e
gabriela <gabriela@sertori.com.br>
2022-06-18 12:39:19 -0300

    fix: comands with quotes working now

--- parser/remove_quotes.c
+++ parser/remove_quotes.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   remove_quotes.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 21:53:46 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/06/15 22:28:57 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/06/18 12:38:46 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ void	remove_quotes(t_ms *ms)
 		{
 			ms->lexer.flag_quote[i] = DOUBLE_QUOTE;
 			remove_quotes_from_str(ms, i, "\"");
-		}	
+		}
 		i++;
 	}
 	ms->lexer.flag_quote[i] = '\0';
@@ -43,9 +43,21 @@ void	remove_quotes(t_ms *ms)
 static void	remove_quotes_from_str(t_ms *ms, int i, char *c)
 {
 	char	*aux;
+	int		char_index;
 
 	aux = ft_strdup(ms->lexer.tokens[i]);
 	free(ms->lexer.tokens[i]);
 	ms->lexer.tokens[i] = ft_strtrim(aux, c);
 	free(aux);
+	char_index = 0;
+	while (ft_strchr(ms->lexer.tokens[i], *c))
+	{
+		char_index = strichar(ms->lexer.tokens[i], 0, *c);
+		while (ms->lexer.tokens[i][char_index])
+		{
+			ms->lexer.tokens[i][char_index] = \
+			ms->lexer.tokens[i][char_index + 1];
+			char_index++;
+		}
+	}
 }
