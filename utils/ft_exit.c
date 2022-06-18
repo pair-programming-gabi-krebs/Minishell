044ad029df91a07e6dc2d17777fff3fd38213287
KrebsCoder <lucasklopess@gmail.com>
2022-06-17 21:54:46 -0300

    add: rl_clear_history

--- utils/ft_exit.c
+++ utils/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 18:34:24 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/06/10 23:05:18 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/06/17 21:54:08 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,6 @@ void	ft_exit(t_ms *ms, int exit_code)
 		free_matrix(ms->cmds.bin);
 	ft_lstclear(ms->list, free);
 	ft_free(ms);
+	rl_clear_history();
 	exit(exit_code);
 }
